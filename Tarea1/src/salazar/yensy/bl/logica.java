7fc1aa38d63bda402067f4404d87100b8604f9b9
Yesala <71746593+Yesala@users.noreply.github.com>
2020-09-30 16:48:08 -0600

    Update logica.java

--- Tarea1/src/salazar/yensy/bl/logica.java
+++ Tarea1/src/salazar/yensy/bl/logica.java
@@ -18,22 +18,28 @@ public class logica {
     private static Persona[] arregloDePersonas = new Persona [100];
     private static CitaVet[] arregloCitasVeterinaria = new CitaVet[50];
     private static ReservaHotel[] arregloReservasHotel = new ReservaHotel [50];
-
     private static int nextPosicion = 0;
 
     public static void procesarOpcion(int opcion) {
         switch (opcion){
             case 1:
+                String nombrePet;
+                boolean repetida = false;
+                do{
                 output.println("Nombre de la mascota: ");
-                String nombrePet = input.next();
+                nombrePet = input.next();
                 for (int i = 0; i < arregloDeMascotas.length; i++){
+                    if (arregloDeMascotas[i] != null){
                         if (arregloDeMascotas[i].getNombrePet().equals(nombrePet)){
                             System.out.println("El nombre ya existe, ingrese otro");
+                            repetida = true;
                         }else{
                             System.out.println("El nombre se ingresó correctamente");
+                            repetida = false;
                         }
-
+                    }
                 }
+                }while (repetida == true);
                 output.println("Nombre del dueño de la mascota: ");
                 String nombreDuenno = input.nextLine();
                 output.println("Ranking de estrellas de la mascota: */**/*** estrellas");
@@ -48,15 +54,23 @@ public class logica {
             case 2:
                 output.println("Nombre y apellidos de la persona: ");
                 String nombre = input.nextLine();
-                output.println("Número de cédula: ");
-                int cedula = input.nextInt();
-                for (int i = 0; i < arregloDePersonas.length; i++){
+                int cedula;
+                boolean repetido = false;
+                do{
+                    output.println("Número de cédula: ");
+                    cedula = input.nextInt();
+                    for (int i = 0; i < arregloDePersonas.length; i++){
+                    if (arregloDePersonas[i] != null){
                         if (arregloDePersonas[i].getCedula() == cedula){
-                            System.out.println("La cédula ya existe, intenté de nuevo");
+                            System.out.println("La cédula ya existe, intente de nuevo");
+                            repetido = true;
                         }else{
                             System.out.println("La cédula se ingresó correctamente");
+                            repetido = false;
                         }
+                    }
                 }
+                }while (repetido == true);
                 output.println("Número de teléfono: ");
                 int telefono = input.nextInt();
                 output.println("Dirección: ");
@@ -69,11 +83,28 @@ public class logica {
                 arregloDePersonas[nextPosicion++]=nuevaPersona;
                 break;
             case 3:
-                output.println("Lista de mascotas de la veterinaria: ");
-                for ( int i =0; i < arregloDeMascotas.length; i++){
-                    output.println("#" + (i+1) + " " + arregloDeMascotas[i]);
+                output.println("Escoja la lista a imprimir: 1. Personas, 2. Mascotas, 3. Citas, 4. Reservaciones");
+                int escogencia = input.nextInt();
+                if (escogencia == 1) {
+                    for (int i = 0; i < arregloDePersonas.length; i++) {
+                        output.println("#" + (i + 1) + " " + arregloDePersonas[i]);
+                    }
                 }
-                break;
+                if (escogencia == 2) {
+                    for (int i = 0; i < arregloDeMascotas.length; i++) {
+                        output.println("#" + (i + 1) + " " + arregloDeMascotas[i]);
+                    }
+                }
+                if (escogencia == 3) {
+                    for (int i = 0; i < arregloCitasVeterinaria.length; i++) {
+                        output.println("#" + (i + 1) + " " + arregloCitasVeterinaria[i]);
+                    }
+                } else {
+                    for (int i = 0; i < arregloReservasHotel.length; i++) {
+                        output.println("#" + (i + 1) + " " + arregloReservasHotel[i]);
+
+                    }
+                }
             case 4:
                 output.println("Nombre de la mascota ");
                 nombrePet = input.next();
